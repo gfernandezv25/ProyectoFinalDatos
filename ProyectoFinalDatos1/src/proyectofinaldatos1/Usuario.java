15dce295fb8f463b1caa3b52d4e3089958f85443
gfernandezv25 <gfernandez10527@ufide.ac.cr>
2023-10-25 16:11:45 -0600

    clases creadas
    nuevas clases

---
+++ ProyectoFinalDatos1/src/proyectofinaldatos1/Usuario.java
@@ -0,0 +1,14 @@
+package proyectofinaldatos1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gabrielfernandezvargas
+ */
+public class Usuario {
+    
+}
